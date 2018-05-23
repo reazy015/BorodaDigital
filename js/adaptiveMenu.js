8a096b7f6a97b6bffd3846a23cbeaaa1a2e5cc96
reazy015 <redshuhart015@gmail.com>
2018-05-23 11:32:26 +0300

    project page, works page

--- js/adaptiveMenu.js
+++ js/adaptiveMenu.js
@@ -5,6 +5,7 @@ window.adaptiveMenu = (function (){
   var callbackToggle = document.querySelector('.callback-btn');
   var headerNav = document.querySelector('.header-nav');
   var callbackBlock = document.querySelector('.callback-form-block');
+  var callbackAdaptiveToggle = document.querySelector('.callback-toggle');
 
   navToggle.addEventListener('click', function() {
     headerNav.classList.toggle('header-nav--open');
@@ -12,6 +13,10 @@ window.adaptiveMenu = (function (){
 
   callbackToggle.addEventListener('click', function() {
     callbackToggle.classList.toggle('callback-btn--open');
+    callbackBlock.classList.toggle('callback-form-block--open');
+  });
+
+  callbackAdaptiveToggle.addEventListener('click', function() {
     callbackBlock.classList.toggle('callback-form-block--open');
   })
 })();
